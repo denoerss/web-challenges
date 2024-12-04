0774ce9f094e1ed12160d014c05d5b58761fd288
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-04 16:21:59 +0100

    Add event listener to buttons

--- 8_js-dom-and-events/2_dark-mode/js/index.js
+++ 8_js-dom-and-events/2_dark-mode/js/index.js
@@ -6,3 +6,14 @@ const bodyElement = document.querySelector('[data-js="body"]');
 const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
 const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
 const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+// add event listener to buttons
+darkModeButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+lightModeButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+toggleButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
