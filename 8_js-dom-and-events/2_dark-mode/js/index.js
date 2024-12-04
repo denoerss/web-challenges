d22d4bd516bfcde25acb1178b7021e2c5e4cf3d5
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-04 16:13:03 +0100

    Select buttons by data-js attribute

--- 8_js-dom-and-events/2_dark-mode/js/index.js
+++ 8_js-dom-and-events/2_dark-mode/js/index.js
@@ -1,3 +1,8 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+// select all 3 buttons w/ data-js attribute
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
