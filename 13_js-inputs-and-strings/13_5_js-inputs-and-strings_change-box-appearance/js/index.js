d8a46e03b01b9b7c232738b0b650237f4e875e31
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-09 17:37:16 +0100

    Add functionality to color slider

--- 13_js-inputs-and-strings/13_5_js-inputs-and-strings_change-box-appearance/js/index.js
+++ 13_js-inputs-and-strings/13_5_js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,9 +1,16 @@
 console.clear();
 
+const box = document.querySelector('[data-js="box"]');
 const inputColor = document.querySelector('[data-js="input-color"]');
 const inputRadius = document.querySelector('[data-js="input-radius"]');
 const inputRotation = document.querySelector('[data-js="input-rotation"]');
 
-inputColor.addEventListener("click", () => {});
+inputColor.addEventListener("input", () => {
+  const colorValue = inputColor.value;
+  const hslColor = `hsl(${colorValue}, 70%, 60%)`;
+  box.style.backgroundColor = hslColor;
+});
+
 inputRadius.addEventListener("click", () => {});
+
 inputRotation.addEventListener("click", () => {});
