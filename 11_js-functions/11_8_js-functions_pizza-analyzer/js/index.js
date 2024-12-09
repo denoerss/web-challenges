742175c19e9f35be4233fb1c7454483f07f1be6d
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-09 09:42:53 +0100

    Add Task 1

--- 11_js-functions/11_8_js-functions_pizza-analyzer/js/index.js
+++ 11_js-functions/11_8_js-functions_pizza-analyzer/js/index.js
@@ -18,9 +18,23 @@ pizzaInput2.addEventListener("input", () => {
 
 // Task 1
 // define the function calculatePizzaGain here
+function calculatePizzaGain(diameter1, diameter2) {
+  // calculate the radius of each pizza (radius = diameter / 2)
+  const radius1 = diameter1 / 2;
+  const radius2 = diameter2 / 2;
+
+  // calculate the area of each pizza (area = PI * radius ^ 2)
+  const area1 = Math.PI * Math.pow(radius1, 2);
+  const area2 = Math.PI * Math.pow(radius2, 2);
+
+  // calculate the gain in area
+  const areaGain = area2 - area1;
+}
 
 // Task 2
 // define the function updatePizzaDisplay here
+function updatePizzaDisplay(pizzaElement, newSize) {}
 
 // Task 3
 // define the function updateOutputColor here
+function updateOutputColor(size1, size2) {}
