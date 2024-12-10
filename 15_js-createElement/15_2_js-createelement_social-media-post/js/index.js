5f0fce418817c3090fce0547c7031bd2b25c0988
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-10 17:41:55 +0100

    Add Solution

--- 15_js-createElement/15_2_js-createelement_social-media-post/js/index.js
+++ 15_js-createElement/15_2_js-createelement_social-media-post/js/index.js
@@ -10,3 +10,40 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+// New Post
+const newPost = document.createElement("article");
+newPost.classList.add("post");
+
+document.body.append(newPost);
+
+// New Post_ Content
+const newPostContent = document.createElement("p");
+newPostContent.textContent = "Ich bin ein neuer Post. Juhuuu!";
+newPostContent.classList.add("post__content");
+
+newPost.append(newPostContent);
+
+// New Post_ Footer
+const newPostFooter = document.createElement("footer");
+newPostFooter.classList.add("post__footer");
+
+newPost.append(newPostFooter);
+
+// New Post_ Footer_ User Name
+const newUserName = document.createElement("span");
+newUserName.textContent = "@denoers";
+newUserName.classList.add("post__username");
+
+newPostFooter.append(newUserName);
+
+// New Post_ Footer_ Like Button
+const newLikeButton = document.createElement("button");
+newLikeButton.addEventListener("click", handleLikeButtonClick);
+newLikeButton.textContent = " ♥ Like";
+
+newLikeButton.classList.add("post__button"); // set class
+newLikeButton.setAttribute("type", "button"); // set attribute "type"
+newLikeButton.setAttribute("data-js", "like-button"); // set attribute "data-js"
+
+newPostFooter.append(newLikeButton);
