b251592d91fd5662c22e5f2c13583af91b18e37a
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-10 15:46:29 +0100

    Append new entry to toast messages container

--- 15_js-createElement/15_1_js-createelement_toast-messages/js/index.js
+++ 15_js-createElement/15_1_js-createelement_toast-messages/js/index.js
@@ -6,6 +6,10 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const newEntry = document.createElement("li");
+  newEntry.classList.add("toast-container__message");
+  newEntry.textContent = "I'm a new toast message.";
+  toastContainer.append(newEntry);
 });
 
 clearButton.addEventListener("click", () => {
