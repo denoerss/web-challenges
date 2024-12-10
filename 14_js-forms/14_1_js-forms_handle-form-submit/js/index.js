5f5ca7c0222b99f10708a19736d94d8287837c39
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-10 12:39:16 +0100

    Add Reset Form

--- 14_js-forms/14_1_js-forms_handle-form-submit/js/index.js
+++ 14_js-forms/14_1_js-forms_handle-form-submit/js/index.js
@@ -11,12 +11,13 @@ form.addEventListener("submit", (event) => {
 
   // V1.1 / List all Elements
   const formElements = event.target.elements;
-  // console.log("FIRST_NAME_", formElements.firstName); // firstName Element
-  // console.log("FIRST_NAME_VALUE_", formElements.firstName.value); // firstName Value
+  // console.log("FORM_ELEMENTS_V1_", formElements); // List all formElements
+  // console.log("FIRST_NAME_ELEMENTS_V1_", formElements.firstName); // List firstName Element
+  // console.log("FIRST_NAME_VALUE_V1_", formElements.firstName.value); // List firstName Value
 
   // V1.2 / List User Input
-  // Personal Information
   const userDataV1 = {
+    // Personal Information
     firstName: formElements.firstName.value,
     lastName: formElements.lastName.value,
     age: formElements.age.value,
@@ -28,13 +29,15 @@ form.addEventListener("submit", (event) => {
     orderDate: formElements.orderDate.value,
     tos: formElements.tos.value,
   };
+
   // V1.3 / Log User Input
   console.log("USER_DATA_V1_(LONG_WAY)_", userDataV1);
 
   // V2 / SHORT WAY
-  // List all Input Values
+
+  // List From Data
   const formData = new FormData(event.target);
-  // console.log("FORM_DATA_", formData);
+  // List User Input
   const userDataV2 = Object.fromEntries(formData);
   console.log("USER_DATA_V2_(SHORT_WAY)_", userDataV2);
 });
