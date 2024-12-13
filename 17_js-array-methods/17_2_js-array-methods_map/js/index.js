1b4d788524856153a70655390ddbe1bef5f8d403
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-13 16:34:50 +0100

    Add lowerCaseAnswers

--- 17_js-array-methods/17_2_js-array-methods_map/js/index.js
+++ 17_js-array-methods/17_2_js-array-methods_map/js/index.js
@@ -24,10 +24,14 @@ const cards = [
   },
 ];
 
-const lowerCaseAnswers = null; // ['as often as you like.', ...]
+const lowerCaseAnswers = cards.map((card) => {
+  return card.answer.toLowerCase(); // ['as often as you like.', ...]
+});
 
 const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
 
 const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
 
 export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
+
+console.log("ANSWERS_", lowerCaseAnswers);
