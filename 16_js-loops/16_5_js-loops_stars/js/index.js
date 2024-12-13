e424c27f2ac1ec6c641fc9e944d7dc5c7f1d1b2a
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-13 12:30:19 +0100

    Add Part 1: Render Stars

--- 16_js-loops/16_5_js-loops_stars/js/index.js
+++ 16_js-loops/16_5_js-loops_stars/js/index.js
@@ -8,6 +8,13 @@ function renderStars() {
 
   // --v-- write or modify code below this line --v--
 
+  // PART 1: RENDER STARS
+  for (let i = 1; i <= 5; i++) {
+    const starEmpty = document.createElement("img");
+    starEmpty.src = "assets/star-empty.svg";
+
+    starContainer.appendChild(starEmpty);
+  }
   // --^-- write or modify code above this line --^--
 }
 
