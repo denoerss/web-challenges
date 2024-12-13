d80083182671e870993c933b46a2ce7b68157a93
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-13 11:24:54 +0100

    Add Part 1: Creating a Select Input

--- 16_js-loops/16_3_js-loops_forin-select-nav/js/index.js
+++ 16_js-loops/16_3_js-loops_forin-select-nav/js/index.js
@@ -21,7 +21,11 @@ select.name = "languages";
 main.append(select);
 
 // --v-- write or modify code below this line --v--
-
+for (const language in languages) {
+  const option = document.createElement("option");
+  option.textContent = languages[language];
+  select.append(option);
+}
 // --^-- write or modify code above this line --^--
 
 // Part 2: Creating a Navigation Bar
