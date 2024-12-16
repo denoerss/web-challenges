e7daba63085c6f3c54f859b7e21db34792cdaf68
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-16 17:28:43 +0100

    Add Task 1: Fetch data from the Star Wars Api

--- 19_js-fetch/19_2_js-fetch_star-wars/js/index.js
+++ 19_js-fetch/19_2_js-fetch_star-wars/js/index.js
@@ -41,6 +41,8 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.py4e.com/api/people");
+  const data = await response.json();
+  console.log("DATA_", data);
 }
