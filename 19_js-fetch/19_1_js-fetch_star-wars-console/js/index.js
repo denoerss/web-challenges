c039287481deb609016b49954d6b72184f88f5ab
Denise Albrecht <denise-albrecht@posteo.de>
2024-12-16 16:54:27 +0100

    Log entire data obect

--- 19_js-fetch/19_1_js-fetch_star-wars-console/js/index.js
+++ 19_js-fetch/19_1_js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,12 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  // log entire data object
+  console.log("ENTIRE DATA OBJECT_", data);
+}
 
 fetchData();
