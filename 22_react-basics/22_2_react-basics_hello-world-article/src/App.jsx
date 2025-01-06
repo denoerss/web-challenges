f0014dd5c74548160326309626b14da47b357fe5
Denise Albrecht <denise-albrecht@posteo.de>
2025-01-06 11:56:59 +0100

    Add solution

--- 22_react-basics/22_2_react-basics_hello-world-article/src/App.jsx
+++ 22_react-basics/22_2_react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,27 @@
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return HelloWorldArticle();
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World Article</h1>
+
+      <h2>Paragraphs</h2>
+      <p>first article paragraph</p>
+      <p>second article paragraph</p>
+      <p>third article paragraph</p>
+
+      <ol>
+        <h2>List Elements</h2>
+        <li>first list element</li>
+        <li>second list element</li>
+        <li>third list element</li>
+      </ol>
+
+      <button>Button</button>
+    </article>
+  );
 }
