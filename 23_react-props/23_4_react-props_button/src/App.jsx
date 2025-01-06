b3b3ac943c7913a0d13220d090823914e6a23174
Denise Albrecht <denise-albrecht@posteo.de>
2025-01-06 18:48:12 +0100

    Add Task 2: Adding a Click Event

--- 23_react-props/23_4_react-props_button/src/App.jsx
+++ 23_react-props/23_4_react-props_button/src/App.jsx
@@ -3,22 +3,49 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div>
-      <Button color="red" disabled text="Disabled" />
-      <Button color="green" text="Active" />
+    <div style={{ display: "flex", textAlign: "center" }}>
+      <Button
+        disabled
+        color="red"
+        text="Disabled"
+        onClick={() => console.log("Button disabled - Click ignored!")}
+      />
+      <Button
+        color="green"
+        text="Active"
+        onClick={() => console.log("Button clicked!")}
+      />
     </div>
   );
 }
 
-function Button({ color, disabled, text }) {
+function Button({ color, disabled, text, onClick }) {
+  const handleClick = () => {
+    if (disabled) {
+      console.log("Button disabled - Click ignored!");
+    } else {
+      onClick();
+    }
+  };
+
   return (
-    <button
+    <div
+      onClick={handleClick}
       style={{
-        color: color,
+        cursor: disabled ? "not-allowed" : "pointer",
       }}
-      disabled={disabled}
     >
-      {text}
-    </button>
+      <button
+        style={{
+          pointerEvents: disabled ? "none" : "auto",
+          color: color,
+          padding: 10,
+          margin: 15,
+        }}
+        disabled={disabled}
+      >
+        {text}
+      </button>
+    </div>
   );
 }
