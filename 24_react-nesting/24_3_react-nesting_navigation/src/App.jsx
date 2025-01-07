5f6df2ab6cd85e43b1ce9e15dfbc1d395845dbcf
Denise Albrecht <denise-albrecht@posteo.de>
2025-01-07 12:26:20 +0100

    Add Task 2: Navigation component / Part 2

--- 24_react-nesting/24_3_react-nesting_navigation/src/App.jsx
+++ 24_react-nesting/24_3_react-nesting_navigation/src/App.jsx
@@ -1,10 +1,39 @@
 import "./styles.css";
+
 import Header from "./components/Header.jsx";
+import Navigation from "./components/Navigation.jsx";
+
+import avatar from "./img/avatar.jpg";
+import logo from "./img/logo.jpg";
 
 export default function App() {
   return (
     <>
-      <Header></Header>
+      <Header>
+        {" "}
+        <a href="#">
+          <img className="round-image" src={logo} alt="logo" />
+        </a>
+        <Navigation>
+          <a className="navigation__link" href="#home">
+            Home
+          </a>
+          <a className="navigation__link" href="#about">
+            About
+          </a>
+          <a className="navigation__link" href="#impressum">
+            Impressum
+          </a>
+        </Navigation>
+        <button
+          type="button"
+          onClick={() => console.log("I could toggle a profile!")}
+          aria-label="toggle profile"
+        >
+          <img className="round-image" src={avatar} alt="avatar" />
+        </button>
+      </Header>
+
       <main>content goes here…</main>
     </>
   );
