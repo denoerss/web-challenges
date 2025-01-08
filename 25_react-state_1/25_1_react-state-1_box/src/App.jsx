b749a644677d91bdec1efc878cc5816cb4301a4c
Denise Albrecht <denise-albrecht@posteo.de>
2025-01-08 18:21:38 +0100

    Add Task Solution

--- 25_react-state_1/25_1_react-state-1_box/src/App.jsx
+++ 25_react-state_1/25_1_react-state-1_box/src/App.jsx
@@ -1,17 +1,18 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let isActive = false;
+  const [active, setActive] = useState(false);
 
   function handleClick() {
-    isActive = !isActive;
+    setActive(!active);
     // Check that the value changes correctly.
-    console.log(isActive);
+    console.log(active);
   }
 
   return (
     <main>
-      <div className={`box ${isActive ? "box--active" : ""}`} />
+      <div className={`box ${active ? "box--active" : ""}`} />
       <button onClick={handleClick}>Activate</button>
     </main>
   );
