47d02104db807aa2501ec735a78f89e1de3e58a8
Denise Albrecht <denise-albrecht@posteo.de>
2025-01-08 10:48:56 +0100

    Add objects to array

--- 26_react-with-arrays/26_1_react-with-arrays_fruits/src/App.jsx
+++ 26_react-with-arrays/26_1_react-with-arrays_fruits/src/App.jsx
@@ -1,7 +1,13 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { id: 1, name: "🍌 banana", color: "yellow" },
+    { id: 2, name: "🍎 apple", color: "red" },
+    { id: 3, name: "🍊 orange", color: "orange" },
+    { id: 4, name: "🍇 grape", color: "purple" },
+    { id: 5, name: "🥝 kiwi", color: "green" },
+  ];
 
   return (
     <div className="app">
