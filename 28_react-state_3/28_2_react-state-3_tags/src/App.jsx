d99f37f1870bb73ba29e27c08dc1b97653211d71
Denise Albrecht <denise-albrecht@posteo.de>
2025-01-09 18:02:53 +0100

    Add solution

--- 28_react-state_3/28_2_react-state-3_tags/src/App.jsx
+++ 28_react-state_3/28_2_react-state-3_tags/src/App.jsx
@@ -6,10 +6,20 @@ import List from "./components/List";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(tag) {
+    setTags([...tags, tag]);
+    // console.log("funktioniert", typeof tag);
+  }
+
+  function handleDeleteTag(tagToDelete) {
+    const tagsToKeep = tags.filter((tag) => tag !== tagToDelete);
+    setTags(tagsToKeep);
+  }
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }
