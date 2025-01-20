3d430fdd788db898d8650ec96da41cf7bd9dea9d
Denise Albrecht <denise-albrecht@posteo.de>
2025-01-20 11:23:17 +0100

    Add solution

--- 33_js-unit-testing/33_1_js-unit-testing_get-animal/index.js
+++ 33_js-unit-testing/33_1_js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimals(animal = "I do not like animals at all!") {
+  if (animal === "cats") {
+    return `I totally love ${animal}!`;
+  }
+  if (animal === "dogs") {
+    return `I like ${animal}!`;
+  }
+  return animal;
+}
--- 33_js-unit-testing/33_1_js-unit-testing_get-animal/index.test.js
+++ 33_js-unit-testing/33_1_js-unit-testing_get-animal/index.test.js
@@ -1,14 +1,16 @@
-import { getAnimal } from "./index.js";
+import { getAnimals } from "./index.js";
 
 test("getAnimals() returns a string 'I totally love cats!' if called with the argument 'cats'.", () => {
-  const result = getAnimal("cats");
+  const result = getAnimals("cats");
   expect(result).toBe("I totally love cats!");
 });
+
 test("getAnimals() returns a string 'I like [animalPluralName]!' if called with a string argument.", () => {
-  const result = getAnimal("dogs");
+  const result = getAnimals("dogs");
   expect(result).toBe("I like dogs!");
 });
+
 test("getAnimals() returns a string 'I do not like animals at all!' if called without arguments.", () => {
-  const result = getAnimal();
+  const result = getAnimals();
   expect(result).toBe("I do not like animals at all!");
 });
