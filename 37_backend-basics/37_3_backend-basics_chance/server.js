37a2a838308a01af0ddaf9fdf10574d44ae574c4
Denise Albrecht <denise-albrecht@posteo.de>
2025-01-24 11:34:53 +0100

    Add solution

--- 37_backend-basics/37_3_backend-basics_chance/server.js
+++ 37_backend-basics/37_3_backend-basics_chance/server.js
@@ -1,17 +1,23 @@
 import { createServer } from "node:http";
 import Chance from "chance";
 
-const chance = newChance(name, age, profession);
-
-const name = chance.name();
-const age = chance.age();
-const profession = chance.profession();
+const chance = new Chance();
 
 export const server = createServer((request, response) => {
+  // inside server for randomly create new Persona on reload
+  const randomPersona = {
+    name: chance.name(),
+    age: chance.age(),
+    profession: chance.profession(),
+  };
+
   if (request.url === "/") {
     response.statusCode = 200;
     response.end(
-      `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+      `Hello, my name is ${randomPersona.name} and I am ${randomPersona.age} years old. I am a ${randomPersona.profession}.`
     );
+  } else {
+    response.statusCode = 404;
+    response.end("Not found");
   }
 });
