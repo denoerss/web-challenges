266188367e9308cbff88487895dce9a5c577e113
Denise Albrecht <denise-albrecht@posteo.de>
2025-01-24 15:33:27 +0100

    Update api/products/index.js

--- 38_backend-api-routes/38_1_backend-api-routes_products/pages/api/products/index.js
+++ 38_backend-api-routes/38_1_backend-api-routes_products/pages/api/products/index.js
@@ -3,5 +3,9 @@ import { getAllProducts } from "@/services/productServices";
 export default function handler(req, res) {
   const allProducts = getAllProducts();
 
+  if (!allProducts) {
+    return res.status(404).json({ status: "Not Found" });
+  }
+
   res.status(200).json(allProducts);
 }
