131b4e668ca809fa968b8a812608c1aeb7994083
Denise Albrecht <denise-albrecht@posteo.de>
2025-01-27 17:16:16 +0100

    Update pages/api/products/[id].js and use the .populate method to integrate the reviews for each product

--- 39_backend-read/backend-read_products/pages/api/products/index.js
+++ 39_backend-read/backend-read_products/pages/api/products/index.js
@@ -3,9 +3,10 @@ import Product from "@/db/models/Product";
 
 export default async function handler(request, response) {
   await dbConnect();
+  const { id } = request.query;
 
   if (request.method === "GET") {
-    const products = await Product.find();
+    const products = await Product.findById(id).populate("reviews");
     return response.status(200).json(products);
   }
   return response.status(405).json({ message: "Method not allowed" });
