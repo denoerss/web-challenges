9b2155eb87530b00b973e0f1698764ac44ae71a5
Denise Albrecht <denise-albrecht@posteo.de>
2025-01-28 15:42:54 +0100

    Add a PUT route

--- 41_backend-update-and-delete/backend-update-and-delete_products/pages/api/products/[id].js
+++ 41_backend-update-and-delete/backend-update-and-delete_products/pages/api/products/[id].js
@@ -17,5 +17,13 @@ export default async function handler(request, response) {
     return;
   }
 
+  if (request.method === "PUT") {
+    const updatedProduct = request.body;
+    await Product.findByIdAndUpdate(id, updatedProduct);
+
+    response.status(200).json({ status: "Product successfully updated." });
+    return;
+  }
+
   response.status(405).json({ status: "Method not allowed." });
 }
